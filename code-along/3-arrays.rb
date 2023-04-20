b83b94f5d9ffaba4ffcc7f48fb9710c7ab38feb1
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-04-20 01:09:07 +0000

    Completed array code-along

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,33 @@
 
 # Arrays are lists of things
 
+foods = [
+    "tacos",
+    "burritos",
+    "quesadillas",
+    "enchiladas"
+]
+
+mixed_array = [
+    "tacos",
+    "sushi",
+    true,
+    4
+]
+
+shopping_lists = [
+    ["diapers","baby wipes","bath toys"],
+    ["coffee","tequila"]
+]
 # Accessing the array
+# puts foods
+# puts mixed_array
+# puts shopping_lists
+# puts foods[0]
+puts shopping_lists[1][0]
 
 # Add to the array
-
+foods.append("oat milk")
+puts foods
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- labs/2-conditionals.rb
+++ labs/2-conditionals.rb
@@ -21,12 +21,19 @@
 
 # 1. set the first die and write it to the screen
 die1 = rand(1..6)
-puts die1
+puts "Die 1 rolled: #{die1}";
 
 # 2. set the second die and write it to the screen
 die2 = rand(1..6)
-puts die2
+puts "Die 2 rolled: #{die2}";
 
 # 3. create a variable for the total and write it to the screen
 total = die1 + die2
-puts "The total is: #{total}"
+puts "Total: #{total}";
+
+if total == 7 || total == 11
+    puts "YOU WIN! MORE MONEY"
+elsif total == 2 || total == 3 || total == 12
+    puts "YOU LOSE :( BYE MONEY"
+else puts "THE POINT IS #{total}"
+end
