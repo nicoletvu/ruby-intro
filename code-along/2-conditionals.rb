1129c65d39f5f13ffbb686d8cb86dce602ec850c
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-04-20 00:23:17 +0000

    Finished code-along for conditionals

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,12 +4,40 @@
 
 # Booleans
 
+puts true
+puts false
+
 # Boolean Expressions
+
+puts 3 > 2
+puts 3 < 2
 
 # If Conditional Logic
 
+if 3 > 2 
+    # do something
+    puts "math works"
+end
+
 # If/Else Conditional Logic
 
+password = "go_warriors"
+user_entered_password = "go_suns"
+if user_entered_password == password
+    puts "Welcome, Warriors fan!"
+else
+    puts "LEAVE"
+end
+
 # Elsif Conditional Logic
+
+home_team = 1
+away_team = 2
+if home_team > away_team
+    puts "You're a winner :)"
+elsif home_team == away_team
+    puts "tied, boring"
+else "You're a loser :("
+end
 
 # Combining Expressions
